7c3bf5b2acc90930b30b1d0069426ae8f1facc32
Vikas <vikask99588@gmail>
2025-02-14 12:16:25 +0530

    Added rsys log to configuration

--- helpers/logger.go
+++ helpers/logger.go
@@ -3,7 +3,6 @@ package helpers
 import (
 	"log"
 	"log/syslog"
-	"os"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -11,31 +10,36 @@ import (
 
 // NewSyslogCore creates a zap core that writes logs to rsyslog
 func NewSyslogCore() zapcore.Core {
-	filename := os.Getenv("LOG_FILENAME")
-	
-	// Connect to local rsyslog using /dev/log
-	writer, err := syslog.New(syslog.LOG_INFO|syslog.LOG_LOCAL0, filename)
-	if err != nil {
-		log.Fatalf("failed to connect to syslog: %v", err)
-	}
+    // Connect to local rsyslog using /dev/log
+    writer, err := syslog.New(syslog.LOG_INFO|syslog.LOG_LOCAL0, "api-gateway")
+    if err != nil {
+        log.Fatalf("failed to connect to syslog: %v", err)
+    }
 
-	syslogWriter := zapcore.AddSync(writer)
+    syslogWriter := zapcore.AddSync(writer)
 
-	encoderConfig := zap.NewProductionEncoderConfig()
-	encoderConfig.TimeKey = "timestamp"
-	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+    encoderConfig := zap.NewProductionEncoderConfig()
+    encoderConfig.TimeKey = ""
+    encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+    encoderConfig.LevelKey = "level"
+    encoderConfig.NameKey = "logger"
+    encoderConfig.CallerKey = "caller"
+    encoderConfig.MessageKey = "msg"
+    encoderConfig.StacktraceKey = "stacktrace"
+    encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+    encoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
 
-	core := zapcore.NewCore(
-		zapcore.NewJSONEncoder(encoderConfig), // Use JSON encoder
-		syslogWriter,
-		zapcore.InfoLevel,
-	)
+    core := zapcore.NewCore(
+        zapcore.NewConsoleEncoder(encoderConfig), // Use console encoder
+        syslogWriter,
+        zapcore.InfoLevel,
+    )
 
-	return core
+    return core
 }
 
 func Logger() *zap.Logger {
-	logger := zap.New(NewSyslogCore(), zap.AddCaller())
-	defer logger.Sync()
-	return logger
+    logger := zap.New(NewSyslogCore(), zap.AddCaller())
+    defer logger.Sync()
+    return logger
 }
